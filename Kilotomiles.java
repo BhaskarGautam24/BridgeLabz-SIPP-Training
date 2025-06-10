a78cfb132e2d23905db43346bd9de5eb17c5d66c
Bhaskar Gautam <premprakash202123@gmail.com>
2025-06-11 01:25:21 +0530

    Add Java Fundamentals Programs

---
+++ Kilotomiles.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Kilotomiles {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Enter distance in kilometers: ");
+    double km = sc.nextDouble();
+
+    double miles = km * 0.621371;
+    System.out.println("Distance in miles: " + miles);
+  }
+}
