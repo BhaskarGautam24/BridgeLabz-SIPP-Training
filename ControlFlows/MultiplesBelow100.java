bc66c4968e1b878a3e810d5b9ede489dd7f92d40
Bhaskar Gautam <premprakash202123@gmail.com>
2025-06-16 11:58:54 +0530

    Added Solution for Multiple Below 100 - ControlFlows

--- ControlFlows/MultiplesBelow100.java
+++ ControlFlows/MultiplesBelow100.java
@@ -18,8 +18,11 @@ public class MultiplesBelow100 {
         for (int i = 100; i >= 1; i--) {
             if (i % number == 0) {
                 System.out.println(i);
+
             }
+
         }
+        sc.close();
     }
 }
 
