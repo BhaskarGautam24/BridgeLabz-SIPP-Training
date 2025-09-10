d0acf77045cdc92eceae28a68b4c56a4d3c9c3e0
Bhaskar Gautam <premprakash202123@gmail.com>
2025-09-10 09:19:47 +0530

    Added solution for DigitalPayment interface with UPI, Credit Card, and wallet payment implementations

---
+++ FunctionalInterface/DigitalPayment.java
@@ -0,0 +1,29 @@
+
+
+public interface DigitalPayment {
+    boolean pay(double amount);
+}
+
+class UPIPayment implements DigitalPayment {
+    @Override
+    public boolean pay(double amount) {
+        System.out.println("Processing UPI payment: $" + amount);
+        return true;
+    }
+}
+
+class CreditCardPayment implements DigitalPayment {
+    @Override
+    public boolean pay(double amount) {
+        System.out.println("Processing Credit Card payment: $" + amount);
+        return true;
+    }
+}
+
+class WalletPayment implements DigitalPayment {
+    @Override
+    public boolean pay(double amount) {
+        System.out.println("Processing Wallet payment: $" + amount);
+        return true;
+    }
+}
