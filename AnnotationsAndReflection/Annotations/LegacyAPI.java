17f620e9b3a809b808b25fe67d216245bc2599c3
Bhaskar Gautam <premprakash202123@gmail.com>
2025-09-11 10:06:45 +0530

    Added solution for LegacyAPI class demonstrating use of @Deprecated annotation for oldFeature method

---
+++ AnnotationsAndReflection/Annotations/LegacyAPI.java
@@ -0,0 +1,18 @@
+package Annotations;
+
+public class LegacyAPI {
+    @Deprecated
+    public void oldFeature() {
+        System.out.println("This is the old way");
+    }
+
+    public void newFeature() {
+        System.out.println("This is the new and improved way");
+    }
+
+    public static void main(String[] args) {
+        LegacyAPI api = new LegacyAPI();
+        api.oldFeature(); // Will show warning
+        api.newFeature();
+    }
+}
